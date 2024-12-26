2417a66bb1b3e39721bc7a126e200723ab053a5d
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-12-26 17:21:41 +0530

    Second commit

--- headerfooter/src/components/Header.jsx
+++ headerfooter/src/components/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
       <div className={`w-full bg-[#58A42A]`}>
         <ul className={`items-center flex text-white w-full`}>
           <li className={`ms-14 md:ms-10`}>
-            <p className={`text-white text-xs py-2`}>
+            <p className={`text-white text-xs py-2 flex`}>
               <i className="fa-solid fa-phone text-xs me-2"></i>
               OUR PHONE NUMBER: +91 90281 44481
             </p>
