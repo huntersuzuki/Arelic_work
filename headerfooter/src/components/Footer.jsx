d428bfa7c16477536b75f9dce9c428c2f1fa6c80
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-12-26 16:20:58 +0530

    First commit

---
+++ headerfooter/src/components/Footer.jsx
@@ -0,0 +1,107 @@
+const Footer = () => {
+  const commonClasses = "hover:text-white hover:underline duration-300";
+  return (
+    <>
+      <footer className="bg-black text-white py-8 underline-offset-8 mt-40 w-full">
+        <div className="container mx-auto md:px-[12%]">
+          <div className="flex flex-col md:flex-row justify-center md:ms-36">
+            <ul className="flex flex-col gap-8 px-5 md:flex md:flex-row md:gap-10 md:text-sm">
+              <li className={`md:w-[30%]`}>
+                <p className={`underline font-bold`}>ABOUT THE STORE</p>
+                <div className={`mt-5 text-[#4c4c4c] flex flex-col gap-5`}>
+                  <p>
+                    Orovita offers premium, eco-friendly oils with a focus on
+                    quality, affordability, and sustainability. Enjoy free home
+                    delivery across Mumbai and save ₹40 with our bottle reuse
+                    policy. Health and environment matter!
+                  </p>
+                  <p>
+                    <i className="fa-solid fa-location-arrow me-2"></i> Turbhe,
+                    Navi-Mumbai, Maharashtra, India - 400705
+                  </p>
+                  <p>
+                    <i className="fa-solid fa-mobile me-2"></i> Phone: +91 90281
+                    44481
+                  </p>
+                </div>
+              </li>
+              <li>
+                <p className={`underline font-bold`}>FEATURED PRODUCTS</p>
+                <div className={`mt-5`}>
+                  <ul className={`flex flex-col gap-2 text-[#4c4c4c]`}>
+                    <li className={commonClasses}>
+                      <a href="#">Edible Oil</a>{" "}
+                    </li>
+                    <li className={commonClasses}>
+                      <a href="#">Hair Care</a>{" "}
+                    </li>
+                    <li className={commonClasses}>
+                      <a href="#">Baby Care</a>{" "}
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <p className={`underline font-bold`}>USEFUL LINKS</p>
+                <div className={`mt-5`}>
+                  <ul className={`flex flex-col gap-2 text-[#4c4c4c]`}>
+                    <li className={commonClasses}>
+                      <a href={`#`}>About Store</a>
+                    </li>
+                    <li className={commonClasses}>
+                      <a href={`#`}>Contact Us</a>
+                    </li>
+                    <li className={commonClasses}>
+                      <a href={`#`}>Latest Post</a>
+                    </li>
+                    <li className={commonClasses}>
+                      <a href={`#`}>Privacy Policy</a>
+                    </li>
+                    <li className={commonClasses}>
+                      <a href={`#`}>Shipping Policy</a>
+                    </li>
+                    <li className={commonClasses}>
+                      <a href={`#`}>Return Policy</a>
+                    </li>
+                    <li className={commonClasses}>
+                      <a href={`#`}>Terms & Condition</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <p className={`underline font-bold`}>SUBSCRIBE NOW</p>
+                <div className={`mt-8 flex flex-col gap-5 outline-0`}>
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder={`Your Email Address`}
+                    className={`bg-black border-b outline-0`}
+                  />
+                  <button
+                    type="submit"
+                    className={`bg-[#58A42A] w-28 h-10 md:w-[55%] md:p-2 hover:bg-opacity-80 duration-300 `}
+                  >
+                    SIGN UP
+                  </button>
+                </div>
+              </li>
+            </ul>
+          </div>
+
+          <div className="border-t border-black my-6"></div>
+          <hr></hr>
+
+          <div className="flex flex-col md:flex-row justify-center items-center text-gray-400 text-sm">
+            <p className={`text-center mt-5`}>
+              &copy; {new Date().getFullYear()} Your Company Name. All rights
+              reserved.
+            </p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
---
+++ headerfooter/src/components/Header.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+
+const Header = () => {
+  const [open, setOpen] = useState(false);
+  const commonNavBarClasses =
+    "relative hover:text-[#58A42A] cursor-pointer duration-300 md:after:content-[''] md:after:block md:after:h-[2px] md:after:w-0 md:after:bg-[#58A42A] md:after:transition-all md:after:duration-300 hover:after:w-full";
+  const underLineAnimate =
+    "after:content-[''] after:block after:h-[2px] after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full";
+  const handleToggleClick = (event) => {
+    event.stopPropagation();
+    setOpen(!open);
+  };
+  return (
+    <>
+      <div className={`w-full bg-[#58A42A] h-10`}>
+        <ul className={`items-center flex text-white w-full`}>
+          <li className={`ms-14 md:ms-10`}>
+            <p className={`text-white text-xs py-2`}>
+              <i className="fa-solid fa-phone text-xs me-2"></i>
+              OUR PHONE NUMBER: +91 90281 44481
+            </p>
+          </li>
+          <li className={`ms-[60%] hidden md:block`}>
+            <p className={`text-xs cursor-pointer ${underLineAnimate}`}>
+              <i className="fa-solid fa-user"></i> MY ACCOUNT
+            </p>
+          </li>
+          <li
+            className={`ms-5 text-xs  hidden md:block cursor-pointer ${underLineAnimate}`}
+          >
+            CART
+          </li>
+          <li
+            className={`ms-5 text-xs  hidden md:block cursor-pointer ${underLineAnimate}`}
+          >
+            CONTACT US
+          </li>
+        </ul>
+      </div>
+      <div className={`px-14`} onClick={() => open && setOpen(false)}>
+        <div className="flex justify-center">
+          <img
+            src="https://orovita.in/cdn/shop/files/orovita-logo-final_402x.png?v=1734232432"
+            alt="logo"
+            className="size-12 md:size-20"
+          />
+        </div>
+        <div className={`md:hidden`}>
+          <i
+            className="fa-solid fa-bars text-2xl -ms-9 md:hidden mt-5"
+            onClick={handleToggleClick}
+          ></i>
+        </div>
+        <ul
+          className={`gap-10  md:flex md:flex-row md:ms-[50px] ${!open ? "-ms-52 flex flex-col mt-8" : "flex flex-col mt-8 mx-0"} duration-300`}
+          onClick={() => event.stopPropagation()}
+        >
+          <li className={commonNavBarClasses}>HOME</li>
+          <li className={commonNavBarClasses}>ABOUT US</li>
+          <li className={commonNavBarClasses}>SHOP</li>
+          <li className={commonNavBarClasses}>BLOGS</li>
+          <li className={commonNavBarClasses}>CONTACT US</li>
+          <li
+            className={`ms-[0%] md:ms-[40%] after:content-[''] after:block after:h-[1px] after:w-0 after:bg-black after:transition-all after:duration-300 hover:after:w-full`}
+          >
+            <a href={`#`} className={`md:italic md:font-thin`}>
+              Login / Register
+            </a>
+          </li>
+          <li className={`hidden md:block`}>
+            <i className="fa-solid fa-magnifying-glass text-lg"></i>
+          </li>
+          <li className={`hidden md:block`}>
+            <i className="fa-regular fa-heart text-lg"></i>
+          </li>
+          <li className={`hidden md:block`}>
+            <i className="fa-solid fa-cart-shopping text-lg"></i>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Header;
