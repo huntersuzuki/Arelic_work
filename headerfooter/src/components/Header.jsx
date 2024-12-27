79ff549ea4b2bf95c6cd9aa82f8458abbcd4dc02
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-12-27 16:37:05 +0530

    Header updates

--- headerfooter/src/components/Header.jsx
+++ headerfooter/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { CiUser } from "react-icons/ci";
 import { CiShoppingCart } from "react-icons/ci";
+import { FaSquarePhone } from "react-icons/fa6";
 const Header = () => {
   const [open, setOpen] = useState(false);
   const commonNavBarClasses =
-    "relative hover:text-[#58A42A] cursor-pointer duration-300 md:after:content-[''] md:after:block md:after:h-[2px] md:after:w-0 md:after:bg-[#58A42A] md:after:transition-all md:after:duration-300 hover:after:w-full";
+    "relative text-sm border-b pb-2 hover:text-[#58A42A] cursor-pointer duration-300 md:after:content-[''] md:after:block md:after:h-[2px] md:after:w-0 md:after:bg-[#58A42A] md:after:transition-all md:after:duration-300 hover:after:w-full";
   const underLineAnimate =
     "after:content-[''] after:block after:h-[2px] after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full";
 
@@ -17,8 +18,10 @@ const Header = () => {
     <>
       <div className="w-full bg-[#58A42A] h-12">
         <div className="grid grid-cols-1 md:grid-cols-3 items-center text-white text-xs py-2 px-4">
-          <div className="flex items-cente justify-center md:ms-10 mt-1.5">
-            <i className="fa-solid fa-phone me-2"></i>
+          <div className="flex items-cente gap-2 justify-center md:ms-10 mt-1.5">
+            <div className={`mt-0.5`}>
+              <FaSquarePhone />
+            </div>
             <p className={`text-center `}>OUR PHONE NUMBER: +91 90281 44481</p>
           </div>
           <div className="hidden md:flex justify-end space-x-5 col-span-2 md:me-10 mt-1.5">
@@ -52,24 +55,32 @@ const Header = () => {
         </div>
         <div
           className={`${
-            open ? "-translate-x-0" : "-translate-x-full"
-          } absolute top-0 left-0 w-full h-screen bg-[#58A42A] text-white z-50 flex flex-col items-center justify-center gap-6 transition-transform duration-500 ease-in-out`}
+            open ? "-translate-x-[45%]" : "-translate-x-full"
+          } absolute top-0 left-0 w-full h-screen bg-black text-white z-50 flex flex-col items-center justify-center gap-6 transition-transform duration-500 ease-in-out`}
           onClick={(e) => e.stopPropagation()}
         >
           <i
             className="fa-solid fa-xmark text-2xl absolute top-5 right-5 cursor-pointer"
             onClick={() => setOpen(false)}
           ></i>
-          <p className={`${commonNavBarClasses} text-lg`}>HOME</p>
-          <p className={`${commonNavBarClasses} text-lg`}>ABOUT US</p>
-          <p className={`${commonNavBarClasses} text-lg`}>SHOP</p>
-          <p className={`${commonNavBarClasses} text-lg`}>BLOGS</p>
-          <p className={`${commonNavBarClasses} text-lg`}>CONTACT US</p>
-          <p>
-            <a href="#" className="italic font-thin text-lg">
-              Login / Register
-            </a>
-          </p>
+          <div className={`flex flex-col gap-5 ms-[48%] relative -top-24`}>
+            <input
+              type="text"
+              name="search"
+              className="w-[92%] py-2 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#58A42A] text-gray-700 "
+              placeholder="Search For Products"
+            />
+            <p className={`${commonNavBarClasses}`}>HOME</p>
+            <p className={`${commonNavBarClasses}`}>ABOUT US</p>
+            <p className={`${commonNavBarClasses} `}>SHOP</p>
+            <p className={`${commonNavBarClasses} `}>BLOGS</p>
+            <p className={`${commonNavBarClasses} `}>CONTACT US</p>
+            <p className={`w-full border-b pb-2`}>
+              <a href="#" className="italic font-thin text-lg pb-2">
+                Login / Register
+              </a>
+            </p>
+          </div>
         </div>
 
         <div className={`hidden md:grid md:grid-cols-2 md:ms-14`}>
