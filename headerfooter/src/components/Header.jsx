01b1d5445de634dcb4652b3d5f3e5c82118a89ed
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-12-27 15:13:28 +0530

    Updated nav bar

--- headerfooter/src/components/Header.jsx
+++ headerfooter/src/components/Header.jsx
@@ -1,91 +1,111 @@
 import { useState } from "react";
-
+import { CiUser } from "react-icons/ci";
+import { CiShoppingCart } from "react-icons/ci";
 const Header = () => {
   const [open, setOpen] = useState(false);
   const commonNavBarClasses =
     "relative hover:text-[#58A42A] cursor-pointer duration-300 md:after:content-[''] md:after:block md:after:h-[2px] md:after:w-0 md:after:bg-[#58A42A] md:after:transition-all md:after:duration-300 hover:after:w-full";
   const underLineAnimate =
     "after:content-[''] after:block after:h-[2px] after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full";
+
   const handleToggleClick = (event) => {
     event.stopPropagation();
     setOpen(!open);
   };
+
   return (
     <>
-      <div className={`w-full bg-[#58A42A]`}>
-        <ul className={`items-center flex text-white w-full`}>
-          <li className={`ms-14 md:ms-10 flex w-full`}>
-            <p className={`text-white text-xs py-2`}>
-              <i className="fa-solid fa-phone text-xs me-2"></i>
-              OUR PHONE NUMBER: +91 90281 44481
-            </p>
-          </li>
-          <li className={`ms-[60%] hidden md:block`}>
-            <p className={`text-xs cursor-pointer ${underLineAnimate}`}>
+      <div className="w-full bg-[#58A42A] h-12">
+        <div className="grid grid-cols-1 md:grid-cols-3 items-center text-white text-xs py-2 px-4">
+          <div className="flex items-cente justify-center md:ms-10 mt-1.5">
+            <i className="fa-solid fa-phone me-2"></i>
+            <p className={`text-center `}>OUR PHONE NUMBER: +91 90281 44481</p>
+          </div>
+          <div className="hidden md:flex justify-end space-x-5 col-span-2 md:me-10 mt-1.5">
+            <p className={`cursor-pointer ${underLineAnimate}`}>
               <i className="fa-solid fa-user"></i> MY ACCOUNT
             </p>
-          </li>
-          <li
-            className={`ms-5 text-xs  hidden md:block cursor-pointer ${underLineAnimate}`}
-          >
-            CART
-          </li>
-          <li
-            className={`ms-5 text-xs  hidden md:block cursor-pointer ${underLineAnimate}`}
-          >
-            CONTACT US
-          </li>
-        </ul>
+            <p className={`cursor-pointer ${underLineAnimate}`}>CART</p>
+            <p className={`cursor-pointer ${underLineAnimate}`}>CONTACT US</p>
+          </div>
+        </div>
       </div>
-      <div className={`px-14`} onClick={() => open && setOpen(false)}>
-        <div className="flex justify-center md:hidden">
-          <img
-            src="https://orovita.in/cdn/shop/files/orovita-logo-final_402x.png?v=1734232432"
-            alt="logo"
-            className="size-12 md:size-20"
-          />
+      <div className="px-4 py-4">
+        <div className="grid grid-cols-3 items-center">
+          <div className="md:hidden">
+            <i
+              className="fa-solid fa-bars text-2xl"
+              onClick={handleToggleClick}
+            ></i>
+          </div>
+          <div className={`ms-[16%]`}>
+            <img
+              src="https://orovita.in/cdn/shop/files/orovita-logo-final_402x.png?v=1734232432"
+              alt="logo"
+              className="w-16 md:hidden"
+            />
+          </div>
+          <div className={`flex gap-5 ms-8 text-3xl md:hidden `}>
+            <CiUser />
+            <CiShoppingCart />
+          </div>
         </div>
-        <div className={`md:hidden`}>
-          <i
-            className="fa-solid fa-bars text-2xl -ms-9 md:hidden mt-5"
-            onClick={handleToggleClick}
-          ></i>
-        </div>
-        <ul
-          className={`gap-10  md:flex md:flex-row md:ms-[50px] ${!open ? "-ms-52 flex flex-col mt-8" : "flex flex-col mt-8 mx-0"} duration-300`}
-          onClick={() => event.stopPropagation()}
+        <div
+          className={`${
+            open ? "-translate-x-0" : "-translate-x-full"
+          } absolute top-0 left-0 w-full h-screen bg-[#58A42A] text-white z-50 flex flex-col items-center justify-center gap-6 transition-transform duration-500 ease-in-out`}
+          onClick={(e) => e.stopPropagation()}
         >
-          <li className={commonNavBarClasses}>HOME</li>
-          <li className={commonNavBarClasses}>ABOUT US</li>
-          <li className={commonNavBarClasses}>SHOP</li>
-          <li className={commonNavBarClasses}>BLOGS</li>
-          <li className={commonNavBarClasses}>CONTACT US</li>
-          <li>
-            <div className="md:flex md:justify-center md:ms-28 hidden">
+          <i
+            className="fa-solid fa-xmark text-2xl absolute top-5 right-5 cursor-pointer"
+            onClick={() => setOpen(false)}
+          ></i>
+          <p className={`${commonNavBarClasses} text-lg`}>HOME</p>
+          <p className={`${commonNavBarClasses} text-lg`}>ABOUT US</p>
+          <p className={`${commonNavBarClasses} text-lg`}>SHOP</p>
+          <p className={`${commonNavBarClasses} text-lg`}>BLOGS</p>
+          <p className={`${commonNavBarClasses} text-lg`}>CONTACT US</p>
+          <p>
+            <a href="#" className="italic font-thin text-lg">
+              Login / Register
+            </a>
+          </p>
+        </div>
+
+        <div className={`hidden md:grid md:grid-cols-2 md:ms-14`}>
+          <ul className="text-center md:flex md:gap-8">
+            <li className={`${commonNavBarClasses} text-[14px] mt-8`}>HOME</li>
+            <li className={`${commonNavBarClasses} text-[14px] mt-8`}>
+              ABOUT US
+            </li>
+            <li className={`${commonNavBarClasses} text-[14px] mt-8`}>SHOP</li>
+            <li className={`${commonNavBarClasses} text-[14px] mt-8`}>BLOGS</li>
+            <li className={`${commonNavBarClasses} text-[14px] mt-8`}>
+              CONTACT US
+            </li>
+            {/*<li className={`text-[13px]`}>*/}
+            {/*  <a href="#" className="italic font-thin">*/}
+            {/*    Login / Register*/}
+            {/*  </a>*/}
+            {/*</li>*/}
+            <li className={`relative left-52`}>
               <img
                 src="https://orovita.in/cdn/shop/files/orovita-logo-final_402x.png?v=1734232432"
                 alt="logo"
-                className="size-12 md:size-16"
+                className="w-24"
               />
-            </div>
-          </li>
-          <li
-            className={`ms-[0%] md:ms-64  after:content-[''] after:block after:h-[1px] after:w-0 after:bg-black after:transition-all after:duration-300 hover:after:w-full`}
-          >
-            <a href={`#`} className={`md:italic md:font-thin`}>
+            </li>
+          </ul>
+
+          <div className="hidden md:flex justify-end space-x-6 px-16 mt-8">
+            <a href="#" className="italic font-thin me-16 text-[13px]">
               Login / Register
             </a>
-          </li>
-          <li className={`hidden md:block`}>
             <i className="fa-solid fa-magnifying-glass text-lg"></i>
-          </li>
-          <li className={`hidden md:block`}>
             <i className="fa-regular fa-heart text-lg"></i>
-          </li>
-          <li className={`hidden md:block`}>
             <i className="fa-solid fa-cart-shopping text-lg"></i>
-          </li>
-        </ul>
+          </div>
+        </div>
       </div>
     </>
   );
