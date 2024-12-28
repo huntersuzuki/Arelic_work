03f152426894fe49e93113f8cdd9aefe86e56f20
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-12-28 16:19:13 +0530

    Header updated

--- headerfooter/src/components/Header.jsx
+++ headerfooter/src/components/Header.jsx
@@ -18,10 +18,10 @@ const Header = () => {
   }, [open]);
 
   const commonNavBarClasses =
-    "relative text-sm  pb-2 hover:text-[#58A42A] cursor-pointer duration-300 md:after:content-[''] md:after:block md:after:h-[2px] md:after:w-0 md:after:bg-[#58A42A] md:after:transition-all md:after:duration-300 hover:after:w-full focus:text-[#58A42A]";
+    "relative text-sm border-b md:border-0 pb-2 hover:text-[#58A42A] cursor-pointer duration-300 md:after:content-[''] md:after:block md:after:h-[2px] md:after:w-0 md:after:bg-[#58A42A] md:after:transition-all md:after:duration-300 hover:after:w-full focus:text-[#58A42A]";
 
-    const animateTopNavElements =
-      "cursor-pointer duration-300 md:after:content-[''] md:after:block md:after:h-[2px] md:after:w-0 md:after:bg-white md:after:transition-all md:after:duration-300 hover:after:w-full";
+  const animateTopNavElements =
+    "cursor-pointer duration-300 md:after:content-[''] md:after:block md:after:h-[2px] md:after:w-0 md:after:bg-white md:after:transition-all md:after:duration-300 hover:after:w-full";
 
   const handleToggleClick = (event) => {
     event.stopPropagation();
