980524f0934f629a74dce6c8c7b201650e03de9e
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-12-31 12:26:43 +0530

    Footer updated by splitting the code into separate components.

--- headerfooter/src/components/footer/Footer.jsx
+++ headerfooter/src/components/footer/Footer.jsx
@@ -1,13 +1,14 @@
 const Footer = () => {
   const commonClasses = "hover:text-white hover:underline duration-300";
+
   return (
     <>
-      <footer className="bg-black text-white mt-5 py-8 underline-offset-8 w-full">
-        <div className="grid grid-cols-1">
-          <div className="">
-            <ul className="flex flex-col gap-8 px-5 md:flex md:flex-row justify-center md:gap-10 md:text-sm">
-              <li className={`md:w-[30%]`}>
-                <p className="relative font-bold after:block after:w-[25px] after:h-[2px] after:bg-white after:mt-1 after:duration-300 text-[16px] ">
+      <footer className="bg-black text-white mt-5 py-8 underline-offset-8 w-full flex flex-col items-center">
+        <div className="w-full max-w-7xl px-6">
+          <div className="w-full">
+            <ul className="flex flex-col gap-8 md:flex md:flex-row md:gap-10 md:text-sm ">
+              <li className={`md:w-[100%]`}>
+                <p className="relative font-bold after:block after:w-[25px] after:h-[2px] after:bg-white after:mt-1 after:duration-300 text-[16px]">
                   ABOUT THE STORE
                 </p>
                 <div className={`mt-5 text-[#4c4c4c] flex flex-col gap-5`}>
@@ -27,30 +28,26 @@ const Footer = () => {
                   </p>
                 </div>
               </li>
-              <li>
-                <p
-                  className={` relative font-bold after:block after:w-[25px] after:h-[2px] after:bg-white after:mt-1 after:duration-300 text-[16px]`}
-                >
+              <li className={`w-[50%]`}>
+                <p className="relative font-bold after:block after:w-[25px] after:h-[2px] after:bg-white after:mt-1 after:duration-300 text-[16px]">
                   FEATURED PRODUCTS
                 </p>
                 <div className={`mt-5`}>
                   <ul className={`flex flex-col gap-2 text-[#4c4c4c]`}>
                     <li className={commonClasses}>
-                      <a href="#">Edible Oil</a>{" "}
+                      <a href="#">Edible Oil</a>
                     </li>
                     <li className={commonClasses}>
-                      <a href="#">Hair Care</a>{" "}
+                      <a href="#">Hair Care</a>
                     </li>
                     <li className={commonClasses}>
-                      <a href="#">Baby Care</a>{" "}
+                      <a href="#">Baby Care</a>
                     </li>
                   </ul>
                 </div>
               </li>
-              <li>
-                <p
-                  className={` relative font-bold after:block after:w-[25px] after:h-[2px] after:bg-white after:mt-1 after:duration-300 text-[16px]`}
-                >
+              <li className={`w-[50%]`}>
+                <p className="relative font-bold after:block after:w-[25px] after:h-[2px] after:bg-white after:mt-1 after:duration-300 text-[16px]">
                   USEFUL LINKS
                 </p>
                 <div className={`mt-5`}>
@@ -79,22 +76,20 @@ const Footer = () => {
                   </ul>
                 </div>
               </li>
-              <li>
-                <p
-                  className={`relative font-bold after:block after:w-[25px] after:h-[2px] after:bg-white after:mt-1 after:duration-300 text-[16px]`}
-                >
+              <li className={`w-full md:w-full`}>
+                <p className="relative font-bold after:block after:w-[25px] after:h-[2px] after:bg-white after:mt-1 after:duration-300 text-[16px]">
                   SUBSCRIBE NOW
                 </p>
                 <div className={`mt-8 flex flex-col gap-5 outline-0`}>
                   <input
                     type="email"
                     name="email"
-                    placeholder={`Your Email Address`}
-                    className={`bg-black border-b outline-0`}
+                    placeholder="Your Email Address"
+                    className="bg-black border-b outline-0 text-white"
                   />
                   <button
                     type="submit"
-                    className={`bg-[#58A42A] w-28 h-10 md:w-[55%] md:p-2 hover:bg-opacity-80 duration-300 `}
+                    className="bg-[#58A42A] w-28 h-10 md:w-[30%] md:p-2 hover:bg-opacity-80 duration-300"
                   >
                     SIGN UP
                   </button>
@@ -104,10 +99,10 @@ const Footer = () => {
           </div>
 
           <div className="border-t border-black my-6"></div>
-          <hr className={`h-[0.5px]`}></hr>
+          <hr className="w-[100%] h-0.5 mx-auto bg-opacity-20 bg-white border-0 rounded" />
 
           <div className="flex flex-col md:flex-row justify-center items-center text-gray-400 text-sm">
-            <p className={`text-center mt-5`}>
+            <p className={` mt-5`}>
               &copy; {new Date().getFullYear()} Your Company Name. All rights
               reserved.
             </p>
