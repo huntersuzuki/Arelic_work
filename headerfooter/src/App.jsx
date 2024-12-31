b57b5eae92296df9ca0f54e3e7b9af805fbc85d0
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-12-31 17:06:50 +0530

    Updated App.jsx file

--- headerfooter/src/App.jsx
+++ headerfooter/src/App.jsx
@@ -1,11 +1,24 @@
 import "./App.css";
 import Header from "./components/header/Header.jsx";
 import Footer from "./components/footer/Footer.jsx";
+import MainImageSlider from "./components/sliders/MainImageSlider.jsx";
+import FeaturedCategories from "./components/featured_categories/FeaturedCategories.jsx";
+import { useEffect, useState } from "react";
+import FeaturedProducts from "./components/featured_products/FeaturedProducts.jsx";
 
 function App() {
+  const [imageData, setImageData] = useState([]);
+  useEffect(() => {
+    fetch("/sliderData.json")
+      .then((response) => response.json())
+      .then((data) => setImageData(data));
+  }, []);
   return (
     <>
       <Header />
+      <MainImageSlider data={imageData} />
+      <FeaturedCategories />
+      <FeaturedProducts />
       <Footer />
     </>
   );
