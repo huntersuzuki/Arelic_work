59d0b21534f737f6fd900d8fc8abea176cfbca61
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-12-31 12:27:30 +0530

    App.jsx file.

--- headerfooter/src/App.jsx
+++ headerfooter/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import Header from "./components/Header.jsx";
-import Footer from "./components/Footer.jsx";
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
 
 function App() {
   return (
