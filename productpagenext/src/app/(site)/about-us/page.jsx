db2f2b07620abe88213b8c0d5a4ba46e28de2abc
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2025-01-03 17:57:40 +0530

    Updated the product

--- productpagenext/src/app/(site)/about-us/page.jsx
+++ productpagenext/src/app/(site)/about-us/page.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import Footer from "@/components/Footer";
+
 const Page = () => {
-  return (
-    <div>
-      About Us
-      <Footer />
-    </div>
-  );
+  return <div>About Us</div>;
 };
 
 export default Page;
